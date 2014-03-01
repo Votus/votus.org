7f2943050a70008ff4a7dc8c233d40ca7f10acef
malex <malex@votus.org>
2014-03-01 15:21:24 -0800

    Modify the AzureEventBus so that more than one subscriber per event can be supported in a durable fashion.

--- Source/Votus.Core/Infrastructure/Azure/ServiceBus/AzureEventBus.cs
+++ Source/Votus.Core/Infrastructure/Azure/ServiceBus/AzureEventBus.cs
@@ -9,7 +9,6 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Votus.Core.Infrastructure.EventSourcing;
 using Votus.Core.Infrastructure.Logging;
-using Votus.Core.Infrastructure.Messaging;
 using Votus.Core.Infrastructure.Serialization;
 using RetryPolicy = Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.RetryPolicy;
 
@@ -19,25 +18,23 @@ namespace Votus.Core.Infrastructure.Azure.ServiceBus
     {
         #region Constants, Variables & Properties
 
-        private const string DefaultEventTopicName   = "AggregateRootEvents";
-        private const string DefaultSubscriptionName = "AzureEventBus";
+        public const string AggregateRootEventTopicName = "AggregateRootEvents";
 
-        private readonly TopicClient         _topicClient;
-        private readonly SubscriptionClient  _subscriptionClient;
-        
+        private readonly string         _connectionString;
+        private readonly TopicClient    _topicClient;
+
+        public Dictionary<Type, EventManager> EventManagers { get; private set; }
+
         [Inject] public ILog        Log         { get; set; }
         [Inject] public ISerializer Serializer  { get; set; }
         
-        public RetryPolicy      RetryPolicy { get; private set; }
-        public HandlerManager   Handlers    { get; private set; }
-
         #endregion
 
         #region Constructors
 
         public AzureEventBus()
         {
-            Handlers = new HandlerManager();
+            EventManagers = new Dictionary<Type, EventManager>();
         }
 
         public
@@ -45,28 +42,12 @@ namespace Votus.Core.Infrastructure.Azure.ServiceBus
             string connectionString)
             : this()
         {
-            _subscriptionClient = SubscriptionClient.CreateFromConnectionString(
-                connectionString:   connectionString,
-                topicPath:          DefaultEventTopicName,
-                name:               DefaultSubscriptionName
-            );
-
-            _subscriptionClient.PrefetchCount                  = 25;
-            _subscriptionClient.MessagingFactory.PrefetchCount = 25;
-
             _topicClient = TopicClient.CreateFromConnectionString(
                 connectionString:   connectionString,
-                path:               DefaultEventTopicName
+                path:               AggregateRootEventTopicName
             );
 
-            RetryPolicy = new RetryPolicy(
-                new ServiceBusTopicErrorDetectionStrategy(
-                    NamespaceManager.CreateFromConnectionString(connectionString),
-                    DefaultEventTopicName,
-                    DefaultSubscriptionName
-                ),
-                new ExponentialBackoff()
-            );
+            _connectionString = connectionString;
         }
 
         #endregion
@@ -78,33 +59,131 @@ namespace Votus.Core.Infrastructure.Azure.ServiceBus
         Subscribe<TEvent>(
             Func<TEvent, Task> handlerAsync)
         {
-            Handlers.Add(handlerAsync);
+            var eventManager = new EventManager<TEvent>(
+                _connectionString, 
+                handlerAsync
+            ) { Log = Log };
+
+            EventManagers.Add(
+                typeof(TEvent), 
+                eventManager
+            );
         }
 
         public
         void
         BeginProcessingEvents()
         {
-            Task.Run(() =>
-                RetryPolicy.ExecuteAction(() =>
-                    // Executing this in a new Task so it can 
-                    // retry without blocking the website from starting
-                    _subscriptionClient.OnMessageAsync(HandleMessageAsync)
-                )
-            );
+            foreach (var eventManager in EventManagers.Values)
+            {
+                var manager = eventManager;
+
+                Task.Run(() => manager.BeginProcessingEvents());
+            }
         }
 
         public 
-        async Task 
-        PublishAsync(IEnumerable<AggregateRootEvent> events)
+        Task 
+        PublishAsync(
+            IEnumerable<AggregateRootEvent> events)
         {
-            foreach (var message in events.Select(ConvertToBrokeredMessage))
-                await _topicClient.SendAsync(message);
+            // Convert the events to Azure Service Bus message type
+            var brokeredMessages = events.Select(@event => new BrokeredMessage(@event) { Label = @event.GetType().Name });
+
+            // Send all the messages at once
+            return _topicClient.SendBatchAsync(brokeredMessages);
         }
 
         #endregion
+    }
 
-        #region Methods
+    public abstract class EventManager
+    {
+        protected RetryPolicy         _retryPolicy;
+        protected SubscriptionClient  _subscriptionClient;
+
+        protected
+        EventManager(
+            string serviceBusConnectionString,
+            string eventName,
+            string eventHandlerName)
+        {
+            _subscriptionClient = SubscriptionClient.CreateFromConnectionString(
+                 connectionString:  serviceBusConnectionString,
+                 topicPath:         AzureEventBus.AggregateRootEventTopicName,
+                 name:              eventHandlerName
+             );
+
+            _subscriptionClient.PrefetchCount                  = 25;
+            _subscriptionClient.MessagingFactory.PrefetchCount = 25;
+
+            var subscriptionFilterByLabel = string.Format(
+                "sys.Label='{0}'", 
+                eventName
+            );
+
+            _retryPolicy = new RetryPolicy(
+                new ServiceBusTopicErrorDetectionStrategy(
+                    namespaceManager:   NamespaceManager.CreateFromConnectionString(serviceBusConnectionString),
+                    topicName:          AzureEventBus.AggregateRootEventTopicName,
+                    subscriptionName:   eventHandlerName,
+                    subscriptionFilter: subscriptionFilterByLabel
+                ),
+                new ExponentialBackoff()
+            );
+        }
+
+        public
+        abstract
+        void
+        BeginProcessingEvents();
+    }
+
+    public class EventManager<TEvent> : EventManager
+    {
+        private readonly Func<TEvent, Task> _asyncEventHandler;
+
+        public ILog Log { get; set; }
+
+        public EventManager(
+            string              serviceBusConnectionString,
+            Func<TEvent, Task>  asyncEventHandler)
+            : base(
+                serviceBusConnectionString, 
+                typeof(TEvent).Name, 
+                GetEventHandlerName(asyncEventHandler))
+        {
+            _asyncEventHandler = asyncEventHandler;
+        }
+
+        private 
+        static 
+        string 
+        GetEventHandlerName(
+            Func<TEvent, Task> asyncEventHandler)
+        {
+            var handlerName = asyncEventHandler
+                .Method
+                .DeclaringType
+                .Name;
+
+            return string.Format(
+                "{0}-{1}", 
+                handlerName, 
+                typeof (TEvent).Name
+            );
+        }
+
+
+        public
+        override 
+        void 
+        BeginProcessingEvents()
+        {
+            _retryPolicy.ExecuteAction(() =>
+                _subscriptionClient.OnMessageAsync(HandleMessageAsync)
+            );
+        }
 
         private
         async Task
@@ -113,45 +192,16 @@ namespace Votus.Core.Infrastructure.Azure.ServiceBus
         {
             var stopwatch = Stopwatch.StartNew();
 
-            var envelope     = message.GetBody<DynamicMessageEnvelope>();
-            var payloadType  = Handlers.GetTypeForName(envelope.PayloadType);
-            var handlerAsync = Handlers.Get(envelope.PayloadType);
-            var payload      = Serializer.Deserialize(envelope.Payload, payloadType);
+            var payload = message.GetBody<TEvent>();
 
-            await handlerAsync(payload);
+            await _asyncEventHandler(payload);
 
             Log.Verbose(
                 "Processed {0} event message {1} in {2}ms",
-                envelope.PayloadType,
+                typeof(TEvent).Name,
                 message.MessageId,
                 stopwatch.ElapsedMilliseconds
             );
         }
-
-        public 
-        BrokeredMessage 
-        ConvertToBrokeredMessage(
-            AggregateRootEvent aggregateRootEvent)
-        {
-            return new BrokeredMessage(
-                ConvertToEnvelope(aggregateRootEvent)
-            );
-        }
-
-        public 
-        DynamicMessageEnvelope 
-        ConvertToEnvelope(
-            AggregateRootEvent aggregateRootEvent)
-        {
-            var payloadTypeName = aggregateRootEvent.GetType().Name;
-            var payload         = Serializer.Serialize(aggregateRootEvent);
-
-            return new DynamicMessageEnvelope {
-                Payload     = payload,
-                PayloadType = payloadTypeName
-            };
-        }
-
-        #endregion
     }
 }
