fcccfa03ff54fda55959870ccc2b81edef815a7d
malex <malex@votus.org>
2014-03-03 20:48:58 -0800

    Add implementation and unit test for the happy path of the new TasksByTaskIdViewManager.

---
+++ Source/Votus.Testing.Unit/Web/Areas/Api/ViewManagers/TasksByTaskIdViewManagerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FakeItEasy;
+using Votus.Core.Infrastructure.Data;
+using Votus.Core.Tasks;
+using Votus.Web.Areas.Api.Models;
+using Votus.Web.Areas.Api.ViewManagers;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace Votus.Testing.Unit.Web.Areas.Api.ViewManagers
+{
+    public class TasksByTaskIdViewManagerTests
+    {
+        private readonly TasksByTaskIdViewManager   _viewManager;
+        private readonly IKeyValueRepository        _fakeRepo;
+
+        public TasksByTaskIdViewManagerTests()
+        {
+            _fakeRepo = A.Fake<IKeyValueRepository>();
+
+            _viewManager = new TasksByTaskIdViewManager {
+                Repository = _fakeRepo
+            };
+        }
+
+        [Fact]
+        public
+        async Task 
+        HandleAsync_TaskCreatedEvent_TaskViewModelSavedToRepo()
+        {
+            // Arrange
+            var taskCreatedEvent = new TaskCreatedEvent {
+                EventSourceId = Guid.NewGuid()
+            };
+
+            // Act
+            await _viewManager.HandleAsync(taskCreatedEvent);
+
+            // Assert
+            A.CallTo(() => 
+                _fakeRepo.SetAsync(
+                    taskCreatedEvent.EventSourceId,
+                    A<TaskViewModel>.That.Matches(task => task.Id == taskCreatedEvent.EventSourceId)
+                )
+            ).MustHaveHappened();
+        }
+    }
+}
---
+++ Source/Votus.Web/Areas/Api/ViewManagers/TasksByTaskIdViewManager.cs
@@ -0,0 +1,28 @@
+﻿using Ninject;
+using Votus.Core.Infrastructure.Data;
+using Votus.Core.Tasks;
+using Votus.Web.Areas.Api.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace Votus.Web.Areas.Api.ViewManagers
+{
+    public class TasksByTaskIdViewManager
+    {
+        [Inject]
+        public IKeyValueRepository Repository { get; set; }
+
+        public 
+        Task 
+        HandleAsync(
+            TaskCreatedEvent taskCreatedEvent)
+        {
+            return Repository.SetAsync(
+                taskCreatedEvent.EventSourceId,
+                new TaskViewModel {
+                    Id    = taskCreatedEvent.EventSourceId, 
+                    Title = taskCreatedEvent.Title
+                }
+            );
+        }
+    }
+}
