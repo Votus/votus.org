a3b3dd4b08539277676ca9145ec0ee4e261045bf
Matthew Thomas <malex@orbiter.me>
2016-08-10 00:24:15 -0700

    Adds a check to ensure that install-dependencies.sh has been run first and to provide a helpful message if is has not.

--- build.sh
+++ build.sh
@@ -12,15 +12,22 @@ echo
 
 PACKAGES_FOLDER='packages'
 PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"
+
+if [ ! -d $PACKAGES_PATH ]; then
+    echo "ERROR: The path $PACKAGES_PATH does not exist, run ./install-dependencies.sh then rerun your previous command."
+    exit 1
+fi
+
 SHELL_SCRIPTS_PATH="$(readlink -f ./src/shell-scripts)"
 NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)"
 source $NODE_TOOLS_PATH
 echo Script $NODE_TOOLS_PATH loaded!
-OUTPUT_FOLDER='out'
-mkdir -p $OUTPUT_FOLDER
-echo Folder \'$OUTPUT_FOLDER\' created/exists!
 initialize_node 'v6.3.1' $PACKAGES_PATH
 echo
-echo Provisioning Node.js modules...
+echo Provisioning Node.js modules needed for building...
 $npm install
 echo Done!
+echo
+OUTPUT_FOLDER='out'
+mkdir -p $OUTPUT_FOLDER
+echo Build output folder \'$OUTPUT_FOLDER\' created/exists!
