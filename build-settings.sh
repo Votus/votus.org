bce415df20ed2820867365a8a800c5a5912b5338
Matthew Thomas <malex@orbiter.me>
2016-08-10 18:12:33 -0700

    Clean up the scripts a tad.

--- build-settings.sh
+++ build-settings.sh
@@ -9,5 +9,5 @@ PACKAGES_FOLDER='packages'                                          # Defines th
 
 # Calculated settings
 export PACKAGES_PATH="$(readlink -f $PACKAGES_FOLDER)"              # Specifies the full local path to the packages folder.
-export SHELL_SCRIPTS_PATH="$(readlink -f ./src/shell-scripts)"      # Specifies the root of all the shell scripts.
-export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)" # Specifies the to node shell tools.
+export SHELL_SCRIPTS_PATH="$(readlink -f ./src/shell-scripts)"      # Specifies the full local path to the shell scripts folder.
+export NODE_TOOLS_PATH="$(readlink -f $SHELL_SCRIPTS_PATH/node.sh)" # Specifies the full local path to the node shell tools.
--- local-setup.sh
+++ local-setup.sh
@@ -6,20 +6,23 @@ echo Starting build dependency provisioning!
 set -o errexit
 set -o nounset
 set -o pipefail
-
 echo
 echo Bash shell configured!
 echo
 
+# Load scripts and settings...
 source build-settings.sh
 echo Build settings loaded!
 
 source $NODE_TOOLS_PATH
 echo Script $NODE_TOOLS_PATH loaded!
 
+# Ensure the packages directory exists.
 mkdir -p $PACKAGES_PATH
 echo Folder \'$PACKAGES_PATH\' created/exists!
 echo
+
+# Ensure the correct version of node is used.
 provision_node 'v6.3.1' $PACKAGES_PATH
 echo
 echo Build dependency provisioning complete!
