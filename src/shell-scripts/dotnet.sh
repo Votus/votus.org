c0bffd902cbcebdc885f1db78c5f1ab8c7d3cddd
Matthew Thomas <malex@orbiter.me>
2016-08-16 20:42:21 -0700

    Attempt to install libunwind8 before dotnet core on linux.

--- src/shell-scripts/dotnet.sh
+++ src/shell-scripts/dotnet.sh
@@ -26,6 +26,7 @@ provision_dotnet() {
             export dotnet=$DOTNET_PACKAGE_PATH/dotnet.exe
             ;;
         $SUPPORTED_OS_LINUX)
+            apt-get libunwind8
             INSTALL_SCRIPT_PATH="$(readlink -f ./src/shell-scripts/dotnet-install.sh)"
             chmod +x $INSTALL_SCRIPT_PATH
             ./src/shell-scripts/dotnet-install.sh -Version $VERSION -InstallDir $DOTNET_PACKAGE_PATH
